3c2632e1e5aeb1ca5790f99c5af1f5fe393dda3b
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-10-20 03:45:31 -0300

    Arrumando Bug da Navbar em Mobiles.
    Alterando Css da Navbar para telas pequenas e telas grandes.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,41 @@
 import './style.css'
-import {IoLogoInstagram, IoLogoRss, IoLogoGithub} from 'react-icons/io5'
-import {PiSignOutBold, PiUserBold, PiHouseBold} from 'react-icons/pi'
+
+import {BsInstagram, BsGithub, BsBrowserChrome, BsFillEmojiSmileUpsideDownFill, BsHouseFill, BsPersonCircle, BsDoorClosedFill} from 'react-icons/bs'
 function Navbar(){
     return (
-        <nav>
-            <div className='social'><a href='https://www.instagram.com/rafaelparroni/'><IoLogoInstagram/></a><a  href='https://github.com/RafaelParoni'><IoLogoGithub/></a><a  href='https://rafaelparoni.vercel.app/'><IoLogoRss/></a></div>
-            <p>Bem vindo: {window.localStorage.getItem('name')}</p>
-            <div className='account'><button onClick={()=> window.location = '/'}><PiHouseBold/></button><button onClick={()=> window.location = '/profile'}><PiUserBold/></button><button onClick={function(){window.localStorage.clear(); window.location = '/'}}><PiSignOutBold/></button></div>
-        </nav>
+        <>
+            <div className='Navbar'>
+                <div className='LargeScreen'>
+                    <div className='LargeSocial'>
+                        <a href='https://github.com/RafaelParoni'><BsGithub/></a>
+                        <a href='https://www.instagram.com/rafaelparroni/'><BsInstagram/></a>
+                        <a href='https://rafaelparoni.vercel.app/'><BsBrowserChrome/></a>
+    
+                    </div>
+                    <div className='LargeUser'>
+                        <p><BsFillEmojiSmileUpsideDownFill/> Olá {window.localStorage.getItem('name')}</p>
+                    </div>
+                    <div className='LargeAccount'>
+                        <button onClick={()=> window.location = '/'}><BsHouseFill/></button>
+                        <button onClick={()=> window.location = '/profile'}><BsPersonCircle/></button>
+                        <button  onClick={()=> {window.localStorage.clear(); window.location = '/login'}}><BsDoorClosedFill/></button>
+                    </div>
+                </div>
+                <div className='SmallScreen'>
+                    <div className='SmallSocial'>
+                        <a href='https://github.com/RafaelParoni'><BsGithub/></a>
+                    </div>
+                    <div className='SmallUser'>
+                        <p>{window.localStorage.getItem('name')}</p>
+                    </div>
+                    <div className='SmallAccount'>
+                        <button onClick={()=> window.location = '/'}><BsHouseFill/></button>
+                        <button onClick={()=> window.location = '/profile'}><BsPersonCircle/></button>
+                        <button onClick={()=> {window.localStorage.clear(); window.location = '/login'}}><BsDoorClosedFill/></button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
